f2d846d2b80e05c082b1e2a96aeaca2a54be95a1
charlesxie <charlesxie@canway.net>
2023-01-13 16:35:11 +0800

    feature: 节点低代码

--- applications/flow/plugin_code/plugin_8.py
+++
@@ -1,5 +0,0 @@
-import time
-class Task:
-    def execute(self):
-        time.sleep(10)
-        return True
--- applications/flow/plugin_code/plugin_9.py
+++
@@ -1,5 +0,0 @@
-import time
-class Task:
-    def execute(self):
-        time.sleep(1)
-        return True
