400086f316a88e6d1f54d55ea1120f56306c0be6
charlesxie <charlesxie@canway.net>
2023-02-08 18:11:13 +0800

    feature: 重新打包

--- web/src/main.js
+++ web/src/main.js
@@ -1,13 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 // 按需引入 bk-magic-vue
 import bkMagic from 'bk-magic-vue'
 
 // 全量引入 bk-magic-vue 样式
 import 'bk-magic-vue/dist/bk-magic-vue.min.css'
+
+import App from './App'
+import router from './router'
 import store from './vuex'
 import axios from 'axios'
 import 'view-design/dist/styles/iview.css'
